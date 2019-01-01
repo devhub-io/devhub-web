c3710f080ffb1f5a3508055029320b35f2f48a11
Robin <sysatom@gmail.com>
2019-01-01 16:07:47 +0800

    developer ui

---
+++ components/general/breadcrumbs/developer.vue
@@ -0,0 +1,22 @@
+<template>
+  <ol class="breadcrumb">
+    <li><nuxt-link to="/">Home</nuxt-link></li>
+    <li><nuxt-link to="/developers">Developers</nuxt-link></li>
+    <li class="active">{{ name }}({{ login }})</li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    login: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
